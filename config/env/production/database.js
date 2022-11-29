dc3f375f340c64363feb31626ad54c9d4fba6498
Dayo <dayo@dojah.io>
2022-11-30 00:21:15 +0100

    added pg lib

---
+++ config/env/production/database.js
@@ -0,0 +1,18 @@
+// path: ./config/env/production/database.js
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: {
+        ca: env("DATABASE_CA"),
+      },
+    },
+    debug: false,
+  },
+});
