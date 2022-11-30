64781578b98a4e5f7a279d81daf0b2811b0c2f68
Dayo <dayo@dojah.io>
2022-11-30 02:28:51 +0100

    fixing build error

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,17 @@
-// path: ./config/env/production/database.js
+// path: ./config/database.js
 
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", "strapi"),
+      password: env("DATABASE_PASSWORD", "strapi"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+        ca: env("DATABASE_CA"),
       },
     },
     debug: false,
