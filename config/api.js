badb870ebbffe1857760198ebeeab56b34d0afd5
Dayo <dayo@dojah.io>
2023-01-11 22:32:35 +0100

    increased the db limit

--- config/api.js
+++ config/api.js
@@ -1,7 +1,10 @@
-module.exports = {
-  rest: {
-    defaultLimit: 25,
-    maxLimit: 100,
-    withCount: true,
+module.exports = ({ env }) => ({
+  responses: {
+    privateAttributes: ["_v", "id", "created_at"],
   },
-};
+  rest: {
+    prefix: "/v1",
+    defaultLimit: 100,
+    maxLimit: 1000,
+  },
+});
